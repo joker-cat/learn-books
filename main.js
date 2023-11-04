b5f574abed36a1bad290cedaf34329ab6825a781
joker <jocker.ss.cat@gmail.com>
2023-11-04 20:02:54 +0800

    install bs5 axios json-server

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+import './assets/resetCSS/reset.css'; //初始化CSS
 import './assets/scss/all.scss';
+
+// BS引用
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap"
+
 
 console.log("Hello world!");
