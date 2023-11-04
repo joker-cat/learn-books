d9dbd858871950626f2e35e750d14a272a5a473c
joker <jocker.ss.cat@gmail.com>
2023-11-04 22:27:35 +0800

    調整main.js import載入順序

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-import './assets/resetCSS/reset.css'; //初始化CSS
-import './assets/scss/all.scss';
-
 // BS引用
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
+import './assets/resetCSS/reset.css'; //初始化CSS
+import './assets/scss/all.scss';
 
 console.log("Hello world!");
