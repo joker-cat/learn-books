3bdee10f14d9c8d72989faec83d8382c4614f62b
joker <jocker.ss.cat@gmail.com>
2023-11-20 22:00:49 +0800

    新增:註冊功能 登入功能 尚未作後續的填寫基本資料

--- js/_sharedvariables.js
+++ js/_sharedvariables.js
@@ -1,9 +1,76 @@
-export function mySessionStorage(inputRes,hrefPage,status) {
-    console.log(status);
-    console.log(789789789);
-    sessionStorage.setItem('token', inputRes.accessToken);
-    sessionStorage.setItem('email', inputRes.user.email);
-    window.location.href = `/learn-project/pages/${hrefPage}.html`;
+import { axios } from "../main.js";
+
+//json-server:port
+export const jsonUrl = "http://localhost:3000";
+
+//註冊
+export function newSignup(inputEmail, inputPassword) {
+    console.log('---帳號沒註冊過---');
+    axios
+        .post(`${jsonUrl}/users`, {
+            email: inputEmail,
+            password: inputPassword,
+            infoConfirm: 0, //1為有填寫完成基本資料
+            resetPassword: 0, //如果是1個話要賺去修改密碼畫面
+            level: 1, //切換家教跟學生的頁面(暫不確定是否用到)
+        })
+        .then((res) => {
+            if (res.status === 201) mySessionStorage(res.data, "setting");
+        })
+        .catch(err => {
+            console.error(err);
+        });
 }
 
-export const jsonUrl = "http://localhost:3000";
+
+
+//檢查信箱是否重複
+export async function alreadySignupEmail(userInput) {
+    function checkEmail(str) {
+        // 返回 Promise
+        return new Promise((resolve, reject) => {
+            axios.get(`${jsonUrl}/users?email=${str}`)
+                .then(res => {
+                    resolve(res.data.length === 1 ? true : false);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        });
+    }
+    try {
+        return await checkEmail(userInput);
+    } catch (error) {
+        console.error(error.message); // 錯誤拋出
+    }
+}
+
+//登入檢查
+export async function comparison(returnPassword) {
+    function checkEmail(str) {
+        // 返回 Promise
+        return new Promise((resolve, reject) => {
+            axios.get(`${jsonUrl}/users?email=${str}`)
+                .then(res => {
+                    resolve(res.data.length === 1 ? res.data[0].password : false);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        });
+    }
+    try {
+        return await checkEmail(returnPassword);
+    } catch (error) {
+        console.error(error.message); // 錯誤拋出
+    }
+}
+
+//設定SessionStorage
+export function mySessionStorage(inputRes, hrefPage) {
+    console.log(inputRes, hrefPage);
+    console.log(inputRes);
+    sessionStorage.setItem('token', inputRes.password);
+    sessionStorage.setItem('email', inputRes.email);
+    window.location.href = `/learn-project/pages/${hrefPage}.html`;
+}
--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,12 @@
 //判斷當前有沒有token來顯示登入前後的navber
 const controlRender = document.querySelector('.control-render');
 const isTokenNull = sessionStorage.getItem("token") !== null;
+window.onload = () => {
+    console.log('window.onload...');
+    console.log(isTokenNull);
+    isTokenNull ? isSignin() : needSignin();
+}
+
 function isSignin() {
     controlRender.innerHTML = `
         <a href="" class="btn-notify h4"><i class="bi bi-bell"></i></a>
