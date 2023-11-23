522ff4346a1c410c64cd011c39683994d3eb9404
TzuChingg <suger5769@gmail.com>
2023-11-24 00:38:32 +0800

    新增:ws測試

--- WebSocketServer.js
+++ WebSocketServer.js
@@ -10,22 +10,22 @@ const server = express().listen(PORT, () =>
 
 const wss = new WebSocketServer({ server });
 wss.on("connection", (ws) => {
-  // 连接成功
+  // 連接成功
   console.log("使用者連接成功");
 
   //接收消息
   ws.on("message", (data) => {
-    //data为客户端发送的消息，将消息原封不动返回回去
+    //data為客户端發送的消息，將消息返回
     console.log("接收消息", data.toString());
     const text = data.toString();
     let clients = wss.clients;
-    //循环，发送消息至每个客户端
-    clients.forEach((client) => {
-      client.send(text)
-    });
+    //循環，发送消息至每个客户端
+    // clients.forEach((client) => {
+    //   client.send(text)
+    // });
   });
 
-  ws.on("close", (code, reason) => {
-    console.log("使用者連接關閉", code, reason);
-  });
+//   ws.on("close", (code, reason) => {
+//     console.log("使用者連接關閉", code, reason);
+//   });
 });
--- js/bookChat.js
+++ js/bookChat.js
@@ -1,11 +1,18 @@
 const host = 'ws://localhost:3000/ws'
 
 const ws = new WebSocket(host)
-ws.onopen = (res) => {
-  console.log(res);
-}
-const Input = document.querySelector('.form-control')
-console.log(Input);
+const sendBtn = document.querySelector('#sendBtn')
+sendBtn.addEventListener('click', () => {
+    ws.onopen = (res) => {
+      console.log(res);
+    }
+    const Input = document.querySelector('.form-control')
+    console.log(Input.value);
+    ws.send(JSON.stringify(Input.value))
+    Input.value = '';
+    1
+    
+})
 
 
 
