1c715428a4885f7c16cd7247780d4a17b7cc81da
TzuChingg <suger5769@gmail.com>
2023-11-27 00:59:29 +0800

    新增:ws身分ok

--- WebSocketServer.js
+++ WebSocketServer.js
@@ -1,31 +1,53 @@
 import express from "express";
-import { WebSocketServer } from 'ws';
+import WebSocket from "./node_modules/ws/index.js";
+import uuidv4 from "./node_modules/uuid/dist/index.js";
 
-
-const PORT = 3000;
+const PORT = 4000;
 
 const server = express().listen(PORT, () =>
   console.log(`Listening on ${PORT}`)
 );
+const wss = new WebSocket.WebSocketServer({ server });
 
-const wss = new WebSocketServer({ server });
 wss.on("connection", (ws) => {
   // 連接成功
   console.log("使用者連接成功");
+  // 聊天室身分
+  const uuid = uuidv4.v4();
+  ws.uuid = uuid;
+  const user = {
+    context: 'user',
+    uuid
+  }
+  ws.send(JSON.stringify(user))
 
   //接收消息
-  ws.on("message", (data) => {
+  ws.on("message", (msg) => {
     //data為客户端發送的消息，將消息返回
-    console.log("接收消息", data.toString());
-    const text = data.toString();
-    let clients = wss.clients;
-    // 循環，发送消息至每个客户端
-    clients.forEach((client) => {
-      client.send(text)
-    });
+    const message = JSON.parse(msg)
+    // console.log(message);
+    const newMessage = {
+        context: 'message',
+        uuid,
+        user: message.user,
+        content: message.content
+    }
+
+    // 發送給每個連線用戶
+    sendAllUser(newMessage)
   });
 
-  ws.on("close", (code, reason) => {
-    console.log("使用者連接關閉", code, reason);
-  });
+//   ws.on("close", (code, reason) => {
+//     console.log(`使用者連接關閉`, code, reason);
+//   });
 });
+
+function sendAllUser(msg){
+    wss.clients.forEach(function (client) {
+        // 已建立連線，  未排除自身    排除自身---->(&& client.uuid !== msg.uuid)
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(msg));
+        }
+      });
+
+}
--- js/bookChat.js
+++ js/bookChat.js
@@ -1,18 +1,84 @@
-const host = "ws://localhost:3000/ws";
+import { axios } from "../main.js";
+const host = "ws://localhost:4000/ws";
 
 const ws = new WebSocket(host);
-const sendBtn = document.querySelector("#sendBtn");
 
+const sendBtn = document.querySelector("#sendBtn");
+// 載入之前紀錄
+
+// 發送訊息
 sendBtn.addEventListener("click", () => {
   const Input = document.querySelector(".form-control");
-  console.log(Input.value);
-  ws.send(JSON.stringify(Input.value));
+  let messageEmit = {
+    user: sessionStorage.getItem("email"),
+    content: Input.value,
+  };
+  ws.send(JSON.stringify(messageEmit));
   Input.value = "";
 });
 
 // 接收server訊息
-ws.onopen = (res) => {
-  ws.onmessage = (event) => {
-    console.log(`[Message from server]:\n %c${event.data}`);
-  };
+ws.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+  if (data.context === "user") {// 連線成功後，會給予連線身分，並存到sessionStorage
+    sessionStorage.setItem("uuid", data.uuid);
+  } else if (data.context === "message" && data.uuid !== sessionStorage.getItem("uuid") ) {// 接收訊息，並判斷身分(別人)
+    let chatModel = `
+    <div class="chatBoardPhoto d-flex h-25">
+        <div class="pictureBackground d-flex justify-content-center align-content-center p-1">
+            <a class="rounded-circle" href="#"><img src="/assets/images/Rabbit.jpg" alt="自拍照"
+                    class="rounded-circle d-flex"></a>
+        </div>
+    </div>
+    <div class="chatBoardSplit ms-2 me-2 ">
+        <div class="chatBoardName mb-1 mt-1">
+            ${data.user}
+        </div>
+        <div class="chatBoardText text-break w-100 bg-white rounded-3 p-3">
+            <P>${data.content}</P>
+        </div>
+    </div>
+    <div class="chatBoardDate d-flex flex-column-reverse">
+        11/08 08:15
+    </div>`;
+    // 傳送至HTML聊天室
+    let cahtBoard = document.querySelector(".chatBoardContent");
+    let newMessage = document.createElement("div");
+    newMessage.className = "chatBoardMessage d-flex mb-3";
+    newMessage.innerHTML = chatModel;
+    cahtBoard.appendChild(newMessage);
+    // 滾動，不確定是不是這樣寫
+    cahtBoard.scrollTo(0, cahtBoard.scrollHeight)
+  } else if (data.context === "message" && data.uuid === sessionStorage.getItem("uuid")) {    // 接收訊息，並判斷身分(自己) 
+    console.log(sessionStorage.getItem("uuid"));
+    let chatModel = `
+    <div class="chatBoardPhoto d-flex h-25">
+        <div class="pictureBackground d-flex justify-content-center align-content-center p-1">
+            <a class="rounded-circle" href="#"><img src="/assets/images/ShibaInu.jpg" alt="自拍照"
+                    class="rounded-circle d-flex"></a>
+        </div>
+    </div>
+    <div class="chatBoardSplit ms-2 me-2">
+        <div class="chatBoardName d-flex flex-row-reverse mb-1 mt-1">
+            ${data.user}
+        </div>
+        <div class="chatBoardText text-break w-100 bg-white rounded-3 p-3">
+            <P>${data.content}
+            </P>
+        </div>
+    </div>
+    <div class="chatBoardDate d-flex flex-column-reverse text-end">
+        11/08 14:36
+    </div>`;
+    // 傳送至HTML聊天室
+    let cahtBoard = document.querySelector(".chatBoardContent");
+    let newMessage = document.createElement("div");
+    newMessage.className = "chatBoardMessage d-flex flex-row-reverse mb-3";
+    newMessage.innerHTML = chatModel;
+    cahtBoard.appendChild(newMessage);
+    // 滾動，不確定是不是這樣寫
+    cahtBoard.scrollTo(0, cahtBoard.scrollHeight)
+   }
 };
+
+
