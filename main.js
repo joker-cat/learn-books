91fab8d2d4cdf5b5a056f666253ee7dcbf915922
Donna Hsiao <donnahsiao@ecloudmobile.com>
2023-11-27 17:08:41 +0800

    fix: 合併衝突

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 //BS的JS
 import "bootstrap";
 
-<<<<<<< HEAD
-import './assets/scss/all.scss';
-=======
 //引用axios
 import axios from 'axios';
->>>>>>> dev
 
 export { axios };
 console.log("Hello world!, start");
