ada2fd7d82f8d11b4d534703c57dee8b30c1045a
TzuChingg <suger5769@gmail.com>
2023-11-29 00:10:16 +0800

    fix:WebSocket、email Server合併

--- server.js
+++ server.js
@@ -43,3 +43,57 @@ app.post('/sendemail', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+
+
+// ===============      Websocket Server        ===============
+import WebSocket from "./node_modules/ws/index.js";
+import uuidv4 from "./node_modules/uuid/dist/index.js";
+
+const PORT = 4000;
+
+const server = express().listen(PORT, () =>
+  console.log(`WS Server is running at http://localhost:${PORT}`)
+);
+const wss = new WebSocket.WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  // 連接成功
+  console.log("使用者連接成功");
+  // 聊天室身分
+  const uuid = uuidv4.v4();
+  ws.uuid = uuid;
+  const user = {
+    context: 'user',
+    uuid,
+  }
+  ws.send(JSON.stringify(user))
+
+  //接收消息
+  ws.on("message", (msg) => {
+    //data為客户端發送的消息，將消息返回
+    const message = JSON.parse(msg)
+    const newMessage = {
+        context: 'message',
+        uuid,
+        user: message.user,
+        content: message.content
+    }
+    // 發送給每個連線用戶
+    sendAllUser(newMessage)
+  });
+
+//  通知使用者關閉連線
+//   ws.on("close", (code, reason) => {
+//     console.log(`使用者連接關閉`, code, reason);
+//   });
+});
+
+function sendAllUser(msg){
+    wss.clients.forEach(function (client) {
+        // 已建立連線，  未排除自身    排除自身---->(&& client.uuid !== msg.uuid)
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(msg));
+        }
+      });
+}
