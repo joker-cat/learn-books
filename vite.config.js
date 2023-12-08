671b0639ee58c762bd7c5f58fee660f8a3917842
joker <jocker.ss.cat@gmail.com>
2023-12-08 23:03:27 +0800

    修正:config.js base

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: "/learn-project/",
+  base: "/learn-books/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
